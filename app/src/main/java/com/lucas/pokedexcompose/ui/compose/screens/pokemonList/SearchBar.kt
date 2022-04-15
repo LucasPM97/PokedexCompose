5b2c4c49d636cea726c3e21426704f6fa1059343
Lucas Perera Montanari <42821366+LucasPM97@users.noreply.github.com>
2022-04-15 11:14:56 -0300

    Cleanup SearchBar components

--- app/src/main/java/com/lucas/pokedexcompose/ui/compose/screens/pokemonList/SearchBar.kt
+++ app/src/main/java/com/lucas/pokedexcompose/ui/compose/screens/pokemonList/SearchBar.kt
@@ -6,17 +6,19 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.lucas.pokedexcompose.ui.theme.PokedexComposeTheme
+import com.lucas.pokedexcompose.ui.theme.PokedexPokemonBackground
 import com.lucas.pokedexcompose.ui.theme.PokedexPokemonStroke
 
 @Composable
@@ -33,36 +35,75 @@ fun SearchBar(
         mutableStateOf(hint != "")
     }
 
-    Box(modifier = modifier) {
-        BasicTextField(
-            value = text,
+    val shape = MaterialTheme.shapes.small
+
+    Box(
+        modifier = modifier
+            .background(
+                PokedexPokemonBackground,
+                shape
+            )
+            .border(
+                BorderStroke(5.dp, PokedexPokemonStroke),
+                shape
+            )
+            .padding(horizontal = 20.dp, vertical = 12.dp)
+    ) {
+
+        Input(
+            text,
             onValueChange = {
                 text = it
                 onSearch(it)
             },
-            maxLines = 1,
-            singleLine = true,
-            textStyle = TextStyle(color = Color.Black),
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.White, MaterialTheme.shapes.small)
-                .border(
-                    BorderStroke(5.dp, PokedexPokemonStroke),
-                    MaterialTheme.shapes.small
-                )
-                .padding(horizontal = 20.dp, vertical = 12.dp)
-                .onFocusChanged {
-                    isHintDisplayed = !it.isFocused
-                }
+            onFocusChanged = {
+                isHintDisplayed = !it.isFocused
+            }
         )
 
         if (isHintDisplayed) {
-            Text(
-                text = hint,
-                style = MaterialTheme.typography.body1,
-                color = Color.LightGray,
-                modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
+            Hint(
+                text = hint
             )
         }
+    }
+}
+
+@Composable
+private fun Input(
+    text: String,
+    onValueChange: (String) -> Unit = {},
+    onFocusChanged: (FocusState) -> Unit = {}
+) {
+    BasicTextField(
+        value = text,
+        onValueChange = onValueChange,
+        maxLines = 1,
+        singleLine = true,
+        textStyle = TextStyle(color = Color.Black),
+        modifier = Modifier
+            .fillMaxWidth()
+            .onFocusChanged {
+                onFocusChanged(it)
+            }
+    )
+}
+
+@Composable
+private fun Hint(text: String) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.body1,
+        color = Color.LightGray
+    )
+}
+
+@Composable
+@Preview
+fun PreviewSearchBar() {
+    PokedexComposeTheme {
+        SearchBar(
+            hint = "Search"
+        )
     }
 }
